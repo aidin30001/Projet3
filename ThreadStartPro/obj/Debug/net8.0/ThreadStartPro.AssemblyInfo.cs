5522bc46a787ca23b61e96a3ce3da2d9fe09245e
айдин шаршекеев <toffiaidin01052004@gmail.com>
2024-10-04 23:06:45 +0600

    thread асинхронный метод

--- ThreadStartPro/obj/Debug/net8.0/ThreadStartPro.AssemblyInfo.cs
+++ ThreadStartPro/obj/Debug/net8.0/ThreadStartPro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThreadStartPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1c7c779a35e3e017935bb82a74ae89c25e1b8dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThreadStartPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThreadStartPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
