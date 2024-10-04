e1c7c779a35e3e017935bb82a74ae89c25e1b8dc
айдин шаршекеев <toffiaidin01052004@gmail.com>
2024-10-04 23:05:20 +0600

    thread асинхронны

---
+++ ThreadStartPro/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ThreadStartPro
+{
+  class Program
+  {
+    static void WriteLine()
+    {
+      while (true)
+      {
+        Console.WriteLine(new string(' ', 20) + "hay");
+      }
+    }
+    public static void Main(string[] args)
+    {
+      ThreadStart writeLine = new ThreadStart(WriteLine);
+      Thread thread = new Thread(writeLine);
+      thread.Start();
+
+      while (true)
+      {
+        Console.WriteLine("Hello");
+      }
+    }
+  }
+}
